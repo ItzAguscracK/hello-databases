b673e4abc4412b853a815200339b2de5874839f9
ItzAguscracK <itzaguscrackyt@gmail.com>
2020-07-07 16:39:13 -0300

    HelloDatabase

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express  = require('express');
+const mongoose = require('mongoose');
+const cors     = require('cors');
+
+const router = require('./routes/api/user');
+
+// puerto y base de datos
+const port = process.env.PORT        || 3000;
+const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
+
+const app = express();
+app.use(cors());
+
+// conexion a la base de datos
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+.catch(err => console.error(`Connection error ${err}`));
+app.use('/api', router);
+
+// el server escucha todo
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ routes/api/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/users', (req, res) => {
+    res.send('todos los usuarios');
+});
+
+router.get('/user/:id', (req, res) => {
+    res.send('el usuaario con id' + req.param.id);
+});
+
+module.exports = router;
