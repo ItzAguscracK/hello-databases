d4f819cddd72012d6c935e8ebaaee6bd24f23cc3
ItzAguscracK <itzaguscrackyt@gmail.com>
2020-07-08 12:27:48 -0300

    Fix Error

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 
+
+const User = require('../../models/User');
+
 // GET a /api/users (todos los usuarios)
 router.get('/users', (req, res) => {
     User.find((err, users) => {
